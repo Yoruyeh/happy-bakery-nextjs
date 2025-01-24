3ec344083c0a156cc681f9bd76513191a0256e63
Yoruyeh <orchardsky31@gmail.com>
2025-01-24 14:46:17 +0800

    style: modify product card style

--- components/card/ProductCard.tsx
+++ components/card/ProductCard.tsx
@@ -21,33 +21,30 @@ interface ProductCardProps {
 
 function ProductCard({ product }: ProductCardProps) {
   return (
-    <div className='bg-bgColor-card h-fit w-full overflow-hidden rounded-xl p-4 shadow-lg'>
-      <div className='relative'>
-        <span className='bg-bgColor-newTag absolute left-0 top-0 z-20 rounded-br-xl rounded-tl-xl px-3 py-1 font-bold text-white'>
-          New
-        </span>
-        <div className='relative w-full overflow-hidden rounded-xl pt-[100%]'>
-          <Image
-            src={product.cover}
-            alt={product.name}
-            fill={true}
-            className='object-cover'
-          />
+    <Link href='/product/category/id'>
+      <div className='h-fit w-full overflow-hidden rounded-xl bg-white p-3 shadow-lg hover:bg-bgColor-card'>
+        <div className='relative'>
+          <span className='absolute left-0 top-0 z-20 rounded-br-xl rounded-tl-xl bg-bgColor-newTag px-3 py-1 font-bold text-white'>
+            New
+          </span>
+          <div className='relative w-full overflow-hidden rounded-xl pt-[100%]'>
+            <Image
+              src={product.cover}
+              alt={product.name}
+              fill={true}
+              className='object-cover'
+            />
+          </div>
         </div>
-      </div>
-      <div className='mt-4 flex flex-col gap-2'>
-        <div className='text-text-brown flex items-center justify-between gap-4'>
+
+        <div className='mt-3 flex flex-col items-center justify-between text-text-brown'>
           <h3 className='text-lg font-bold'>{product.name}</h3>
           <span className='relative bottom-1 text-lg font-bold'>
             ${product.priceRegular}
           </span>
         </div>
-
-        <button className='bg-bgColor-secondaryBtn hover:bg-bgColor-secondaryHover text-text-darkGray rounded-md p-2 font-medium hover:text-white'>
-          <Link href='/product/category/id'>VIEW PRODUCT</Link>
-        </button>
       </div>
-    </div>
+    </Link>
   );
 }
 
