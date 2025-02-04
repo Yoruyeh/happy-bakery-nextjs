0ac6199cd00d1658e78f62b4226d2b2e78cb8d06
Yoruyeh <orchardsky31@gmail.com>
2025-02-04 15:19:53 +0800

    style: modify review card style

--- components/card/ReviewCard.tsx
+++ components/card/ReviewCard.tsx
@@ -15,10 +15,16 @@ interface ReviewCardProps {
 function ReviewCard({ review }: ReviewCardProps) {
   return (
     <div className='flex h-fit w-full flex-col rounded-lg bg-white shadow-lg'>
-      <div className='flex w-full justify-between gap-4 p-8'>
-        <div className='text-text-darkGray flex flex-col gap-2'>
-          <h2 className='text-xl font-semibold'>{review.title}</h2>
-          <p className='text-text-lightGray'>{review.content}</p>
+      <div className='grid grid-cols-4 gap-2 p-4'>
+        <div className='col-span-3 flex flex-col gap-2 text-text-darkGray'>
+          <h2 className='line-clamp-1 text-xl font-semibold'>{review.title}</h2>
+          <p className='group relative h-[96px] cursor-help text-text-lightGray'>
+            <span className='line-clamp-4'>{review.content}</span>
+            {/* Tooltip */}
+            <span className='absolute right-0 top-16 z-50 hidden w-60 rounded-md border bg-white p-3 text-sm shadow-lg group-hover:block'>
+              {review.content}
+            </span>
+          </p>
           <p className='flex items-center gap-2'>
             <StarIcon className='h-6 w-6 text-yellow-400' />
             <StarIcon className='h-6 w-6 text-yellow-400' />
@@ -28,13 +34,15 @@ function ReviewCard({ review }: ReviewCardProps) {
             <span className='mt-1 font-medium'>5.0</span>
           </p>
         </div>
-        <div className='relative h-16 w-16 overflow-hidden rounded-full'>
-          <Image
-            src={review.avatar}
-            alt='reviewer-avatar'
-            fill={true}
-            className='object-cover'
-          />
+        <div className='col-span-1 flex justify-end'>
+          <div className='relative h-16 w-16 overflow-hidden rounded-full'>
+            <Image
+              src={review.avatar}
+              alt='reviewer-avatar'
+              fill={true}
+              className='object-cover'
+            />
+          </div>
         </div>
       </div>
       <div className='relative overflow-hidden rounded-b-lg pt-[75%]'>
