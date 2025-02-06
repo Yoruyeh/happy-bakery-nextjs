b17b730aca516e0c709b60850c86567485421b2d
Yoruyeh <orchardsky31@gmail.com>
2025-02-06 18:09:34 +0800

    feat: add tanstack query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import { cookies } from 'next/headers';
 import { ProductService } from '@/api/services/Product';
+import Providers from '@/provider/provider';
 
 const josefinSans = Josefin_Sans({
   variable: '--font-josefin-sans',
@@ -33,16 +34,18 @@ export default async function RootLayout({
 
   return (
     <html lang='en'>
-      <body
-        className={`${josefinSans.variable} ${openSans.variable} mx-auto flex h-fit min-h-screen w-screen flex-col overflow-x-hidden font-sans antialiased`}
-      >
-        <div className='h-14 w-full md:h-20 lg:h-24' />
-        <Header token={token} categories={categories} />
-        <main className='flex flex-1 flex-col px-6 py-4 md:px-8 md:py-8 lg:px-10 lg:py-10 lg:pr-12'>
-          {children}
-        </main>
-        <Footer categories={categories} />
-      </body>
+      <Providers>
+        <body
+          className={`${josefinSans.variable} ${openSans.variable} mx-auto flex h-fit min-h-screen w-screen flex-col overflow-x-hidden font-sans antialiased`}
+        >
+          <div className='h-14 w-full md:h-20 lg:h-24' />
+          <Header token={token} categories={categories} />
+          <main className='flex flex-1 flex-col px-6 py-4 md:px-8 md:py-8 lg:px-10 lg:py-10 lg:pr-12'>
+            {children}
+          </main>
+          <Footer categories={categories} />
+        </body>
+      </Providers>
     </html>
   );
 }
