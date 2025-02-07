01da4fac44e45160c33070012460e5005f1e96f0
Yoruyeh <orchardsky31@gmail.com>
2025-02-07 21:04:54 +0800

    style: add page loader component

---
+++ components/spinner/PageLoader.tsx
@@ -0,0 +1,9 @@
+function PageLoader() {
+  return (
+    <div className='fixed inset-0 z-10 flex items-center justify-center bg-black opacity-60'>
+      <span className='loader'></span>
+    </div>
+  );
+}
+
+export default PageLoader;
