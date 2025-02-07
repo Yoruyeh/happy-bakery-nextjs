32b9994cd65c63a6808b6589c63b3adfacfc46d2
Yoruyeh <orchardsky31@gmail.com>
2025-02-07 21:31:21 +0800

    feat: services and types of get orders, get order details and create order

---
+++ api/services/Order.ts
@@ -0,0 +1,24 @@
+import { fetchWrapper } from '../config/config';
+import {
+  GetOrdersResponse,
+  GetOrderDetailResponse,
+  CreateOrderResponse,
+  CreateOrderPayload,
+} from '../types/order';
+
+export const OrderService = {
+  getOrders: async () => {
+    return fetchWrapper<GetOrdersResponse>('/users/orders');
+  },
+
+  getOrderDetail: async (id: number) => {
+    return fetchWrapper<GetOrderDetailResponse>(`/orders/${id}`);
+  },
+
+  createOrder: async (payload: CreateOrderPayload) => {
+    return fetchWrapper<CreateOrderResponse>('/orders', {
+      method: 'POST',
+      body: JSON.stringify(payload),
+    });
+  },
+};
---
+++ api/types/order.ts
@@ -0,0 +1,105 @@
+export interface UserOrder {
+  id: number;
+  order_date: string;
+  total_price: number;
+  status: string;
+}
+
+export interface ProductInOrder {
+  name: string;
+  cover: string;
+}
+
+export interface OrderItem {
+  quantity: number;
+  price_each: number;
+  Product: ProductInOrder;
+}
+
+export interface UserOrderDetail {
+  id: number;
+  order_date: string;
+  total_price: number;
+  status: string;
+  customer_name: string;
+  email: string;
+  address: string;
+  phone: string;
+  shipping_method: string;
+  payment_method: string;
+  OrderItems: OrderItem[];
+  items_count: number;
+  shipping_fee: number;
+}
+
+export interface NewOrder {
+  id: number;
+  userId: number;
+  status: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  address: string;
+  orderDate: string;
+  totalPrice: number;
+  paymentMethod: string;
+  shippingMethod: string;
+  updatedAt: string;
+  createdAt: string;
+}
+
+export interface NewOrderItem {
+  id: number;
+  productId: number;
+  quantity: number;
+  priceEach: number;
+  orderId: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CreateOrderItem {
+  id: number;
+  quantity: number;
+  price: number;
+}
+
+export interface ShipmentData {
+  email: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  phone: string;
+  shippingMethod: string;
+}
+
+export interface PaymentData {
+  paymentMethod: string;
+}
+
+export interface CreateOrderPayload {
+  orderItems: CreateOrderItem[];
+  total: number;
+  shipment: ShipmentData;
+  payment: PaymentData;
+}
+
+export interface GetOrdersResponse {
+  status: 'success' | 'error';
+  message: string;
+  userOrders: UserOrder[];
+}
+
+export interface GetOrderDetailResponse {
+  status: 'success' | 'error';
+  message: string;
+  order: NewOrder;
+}
+
+export interface CreateOrderResponse {
+  status: 'success' | 'error';
+  message: string;
+  newOrder: NewOrder;
+  newOrderItem: NewOrderItem[];
+}
