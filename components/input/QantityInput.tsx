28c163c46f64c360821eb6a653fab52263ee7304
Yoruyeh <orchardsky31@gmail.com>
2025-02-07 12:59:29 +0800

    style: add delete cart item icon in quantity input

--- components/input/QantityInput.tsx
+++ components/input/QantityInput.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { twMerge } from 'tailwind-merge';
-import { MinusIcon, PlusIcon } from '@heroicons/react/24/outline';
+import { MinusIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
 
 interface QantityInputProps {
@@ -30,38 +30,43 @@ function QantityInput({ quantity }: QantityInputProps) {
   }
 
   return (
-    <div className='flex h-fit w-fit items-center [&>*:not(:first-child)]:-ml-[1px]'>
-      <button
-        disabled={inputValue === 1}
-        className={twMerge(
-          'flex h-8 items-center justify-center border border-gray-400 bg-blue-50 px-2 text-black lg:h-10 lg:text-base',
-          // Disabled Style
-          inputValue === 1 && 'bg-gray-300 text-gray-500'
-        )}
-        onClick={() => QuantityControlHandler('minus')}
-      >
-        <MinusIcon className='h-[14px] w-[14px] md:h-4 md:w-4' />
-      </button>
-      <input
-        type='number'
-        min={1}
-        max={99}
-        value={inputValue}
-        className={twMerge(
-          'no-spinners-input relative z-10 h-8 w-10 rounded-none border border-gray-400 bg-white px-0 text-center text-sm font-medium text-black focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 md:text-base lg:h-10'
-        )}
-        onChange={inputChangeHandler}
-      />
-      <button
-        disabled={inputValue === 99}
-        className={twMerge(
-          'flex h-8 items-center justify-center border border-gray-400 bg-blue-50 px-2 text-black lg:h-10 lg:text-base',
-          // Disabled Style
-          inputValue === 99 && 'bg-gray-300 text-gray-500'
-        )}
-        onClick={() => QuantityControlHandler('plus')}
-      >
-        <PlusIcon className='h-[14px] w-[14px] md:h-4 md:w-4' />
+    <div className='flex items-center gap-3'>
+      <div className='flex h-fit w-fit items-center [&>*:not(:first-child)]:-ml-[1px]'>
+        <button
+          disabled={inputValue === 1}
+          className={twMerge(
+            'flex h-8 items-center justify-center border border-gray-400 bg-blue-50 px-2 text-black lg:h-10 lg:text-base',
+            // Disabled Style
+            inputValue === 1 && 'bg-gray-300 text-gray-500'
+          )}
+          onClick={() => QuantityControlHandler('minus')}
+        >
+          <MinusIcon className='h-[14px] w-[14px] md:h-4 md:w-4' />
+        </button>
+        <input
+          type='number'
+          min={1}
+          max={99}
+          value={inputValue}
+          className={twMerge(
+            'no-spinners-input relative z-10 h-8 w-10 rounded-none border border-gray-400 bg-white px-0 text-center text-sm font-medium text-black focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 md:text-base lg:h-10'
+          )}
+          onChange={inputChangeHandler}
+        />
+        <button
+          disabled={inputValue === 99}
+          className={twMerge(
+            'flex h-8 items-center justify-center border border-gray-400 bg-blue-50 px-2 text-black lg:h-10 lg:text-base',
+            // Disabled Style
+            inputValue === 99 && 'bg-gray-300 text-gray-500'
+          )}
+          onClick={() => QuantityControlHandler('plus')}
+        >
+          <PlusIcon className='h-[14px] w-[14px] md:h-4 md:w-4' />
+        </button>
+      </div>
+      <button>
+        <TrashIcon className='h-4 w-4 text-text-darkGray lg:h-5 lg:w-5' />
       </button>
     </div>
   );
